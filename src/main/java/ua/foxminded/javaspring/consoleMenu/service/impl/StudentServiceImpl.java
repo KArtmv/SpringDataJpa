620f20a7fc4454c452977efb00a0b4c01caab336
”Kyrylo <kirillartemov1993@gmail.com>
2024-02-08 13:47:34 +0100

    Refactor StudentServiceImpl -Remove StudentToCourseDAO dependency -Add Logger -Refactor addStudentToCourse()  Move checking is exist course from if, add throw exception if is not  Add checking is student is already enrollment to given course  Replace saving course to student by student util method and save it by  studentDAO instead studentToCourseDAO(studentToCourse) -Refactor removeStudentFromCourse()  Replace void to return boolean statement  Remove checking is course relate to given student  Add into 'if' checking is course removed from student Set  Replace save by studentDAO instead studentToCourseDAO

--- src/main/java/ua/foxminded/javaspring/consoleMenu/service/impl/StudentServiceImpl.java
+++ src/main/java/ua/foxminded/javaspring/consoleMenu/service/impl/StudentServiceImpl.java
@@ -1,31 +1,31 @@
 package ua.foxminded.javaspring.consoleMenu.service.impl;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
 import org.springframework.stereotype.Service;
 import ua.foxminded.javaspring.consoleMenu.dao.CourseDAO;
 import ua.foxminded.javaspring.consoleMenu.dao.GroupDAO;
-import ua.foxminded.javaspring.consoleMenu.dao.StudentAtCourseDAO;
 import ua.foxminded.javaspring.consoleMenu.dao.StudentDAO;
 import ua.foxminded.javaspring.consoleMenu.exception.InvalidIdException;
+import ua.foxminded.javaspring.consoleMenu.model.Course;
 import ua.foxminded.javaspring.consoleMenu.model.Student;
-import ua.foxminded.javaspring.consoleMenu.model.StudentAtCourse;
 import ua.foxminded.javaspring.consoleMenu.service.StudentService;
-
-import java.util.List;
-import java.util.Objects;
 
 @Service
 public class StudentServiceImpl implements StudentService {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(StudentServiceImpl.class);
+
+
     private StudentDAO studentDAO;
-    private StudentAtCourseDAO studentAtCourseDAO;
     private GroupDAO groupDAO;
     private CourseDAO courseDAO;
 
     @Autowired
-    public StudentServiceImpl(StudentDAO studentDAO, StudentAtCourseDAO studentAtCourseDAO, GroupDAO groupDAO, CourseDAO courseDAO) {
+    public StudentServiceImpl(StudentDAO studentDAO, GroupDAO groupDAO, CourseDAO courseDAO) {
         this.studentDAO = studentDAO;
-        this.studentAtCourseDAO = studentAtCourseDAO;
         this.groupDAO = groupDAO;
         this.courseDAO = courseDAO;
     }
@@ -45,34 +45,37 @@ public class StudentServiceImpl implements StudentService {
     }
 
     @Override
-    public boolean addStudentToCourse(StudentAtCourse studentAtCourse) {
-        if (courseDAO.existsById(studentAtCourse.getCourse().getId())) {
-            return studentAtCourseDAO.save(studentAtCourse).getId() != null;
-        } else {
-            throw new InvalidIdException("Not found course with received ID: " + studentAtCourse.getCourse().getId());
-        }
+    public boolean addStudentToCourse(Student student, Course course) {
+            Course filalCourse = courseDAO.findById(course.getId()).orElseThrow(() ->
+                    new InvalidIdException("Not found course with received ID: " + course.getId()));
+
+            if (student.getCourses().stream().anyMatch(c -> c.equals(filalCourse))) {
+                LOGGER.debug("Student id: {} is already enrolled to course id: {}.", student.getId(), course.getId());
+            } else {
+                if (student.addCourse(course)) {
+                    return studentDAO.save(student).getId() != null;
+                } else {
+                    LOGGER.error("Add course id: {} to student id: {} is faled.", student.getId(), course.getId());
+                }
+            }
+        return false;
     }
 
+
     @Override
-    public void removeStudentFromCourse(StudentAtCourse studentAtCourse) {
-        if (getAllCoursesOfStudent(studentAtCourse.getStudent()).stream()
-                .anyMatch(id -> Objects.equals(id.getId(), studentAtCourse.getId()))) {
-            studentAtCourseDAO.delete(studentAtCourse);
-        } else {
-            throw new InvalidIdException(String.format(
-                    "Received enrollment ID: %s, is not found or not relate to given student: ID: %s ",
-                    studentAtCourse.getId(), studentAtCourse.getStudent().getId()));
-        }
+    public boolean removeStudentFromCourse(Student student, Course course) {
+         if (student.removeCourse(courseDAO.findById(course.getId()).orElseThrow(() ->
+                 new InvalidIdException(String.format("Received course id: %s, is not found.", course.getId()))))) {
+                return studentDAO.save(student).getId() != null;
+         } else {
+             LOGGER.info("Course with given id: {} is not relate to student id: {}", course.getId(), student.getId());
+             return false;
+         }
     }
 
     @Override
     public Student getStudent(Student student) {
         return studentDAO.findById(student.getId()).orElseThrow(() ->
                 new InvalidIdException("Not found student with given ID: " + student.getId()));
-    }
-
-    @Override
-    public List<StudentAtCourse> getAllCoursesOfStudent(Student student) {
-        return studentAtCourseDAO.findAllStudentCourses(student);
     }
 }
