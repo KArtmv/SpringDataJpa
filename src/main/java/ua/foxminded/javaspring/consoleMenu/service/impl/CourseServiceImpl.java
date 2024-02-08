44d673ea4873a5b56a9000c31c86bf308c392001
”Kyrylo <kirillartemov1993@gmail.com>
2024-02-08 11:19:32 +0100

    Refactor CourseServiceImpl -Remove StudentToCourse dependency -Remove allStudentsFromCourse() implementation -Implement getCourse()

--- src/main/java/ua/foxminded/javaspring/consoleMenu/service/impl/CourseServiceImpl.java
+++ src/main/java/ua/foxminded/javaspring/consoleMenu/service/impl/CourseServiceImpl.java
@@ -3,10 +3,8 @@ package ua.foxminded.javaspring.consoleMenu.service.impl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import ua.foxminded.javaspring.consoleMenu.dao.CourseDAO;
-import ua.foxminded.javaspring.consoleMenu.dao.StudentAtCourseDAO;
 import ua.foxminded.javaspring.consoleMenu.exception.InvalidIdException;
 import ua.foxminded.javaspring.consoleMenu.model.Course;
-import ua.foxminded.javaspring.consoleMenu.model.StudentAtCourse;
 import ua.foxminded.javaspring.consoleMenu.service.CourseService;
 
 import java.util.List;
@@ -14,20 +12,17 @@ import java.util.List;
 @Service
 public class CourseServiceImpl implements CourseService {
 
-    private StudentAtCourseDAO studentAtCourseDAO;
     private CourseDAO courseDAO;
 
     @Autowired
-    public CourseServiceImpl(StudentAtCourseDAO studentAtCourseDAO, CourseDAO courseDAO) {
-        this.studentAtCourseDAO = studentAtCourseDAO;
+    public CourseServiceImpl(CourseDAO courseDAO) {
         this.courseDAO = courseDAO;
     }
 
     @Override
-    public List<StudentAtCourse> allStudentsFromCourse(Course course) {
-        return studentAtCourseDAO.allStudentsFromCourse(
-                courseDAO.findById(course.getId()).orElseThrow(() ->
-                        new InvalidIdException("No founded course by given ID: " + course.getId())));
+    public Course getCourse(Course course) {
+        return courseDAO.findById(course.getId()).orElseThrow(() ->
+                        new InvalidIdException("No founded course by given ID: " + course.getId()));
     }
 
     @Override
