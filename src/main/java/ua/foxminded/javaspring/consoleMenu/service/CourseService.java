255a1fc99d9cf8bcabcb0781653eaacdcd4a45a6
”Kyrylo <kirillartemov1993@gmail.com>
2024-02-08 11:16:00 +0100

    Refactor CourseService -Remove allStudentsFromCourse() -Create getCource()

--- src/main/java/ua/foxminded/javaspring/consoleMenu/service/CourseService.java
+++ src/main/java/ua/foxminded/javaspring/consoleMenu/service/CourseService.java
@@ -1,12 +1,11 @@
 package ua.foxminded.javaspring.consoleMenu.service;
 
 import ua.foxminded.javaspring.consoleMenu.model.Course;
-import ua.foxminded.javaspring.consoleMenu.model.StudentAtCourse;
 
 import java.util.List;
 
 public interface CourseService {
-    List<StudentAtCourse> allStudentsFromCourse(Course course);
+    Course getCourse(Course course);
 
     List<Course> getAllCourses();
 }
